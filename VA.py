2f0ac73ab7e08e203ca4590d1b362e45537cdd46
Michael Xie <mxie0114@gmail.com>
2024-08-15 01:23:45 -0500

    initial commit

---
+++ VA.py
@@ -0,0 +1,21 @@
+import subprocess
+import wolframalpha
+import pyttsx3
+import tkinter
+import json
+import random
+import operator
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import winshell
+import feedparser
+import smtplib
+import ctypes
+import time
+import requests
+import shutil
+from twilio.rest import Client
+from clint.textui import progress
